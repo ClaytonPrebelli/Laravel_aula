b46163f2f56849be3289af78a6df8810e527c2d3
Clayton Prebelli <clayton.pires@aluno.unip.br>
2021-06-14 19:40:29 -0300

    criado product seeders

--- marketplace/database/seeders/StoreTableSeeder.php
+++ marketplace/database/seeders/StoreTableSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Product;
+use App\Models\Store;
 use Illuminate\Database\Seeder;
 
 class StoreTableSeeder extends Seeder
@@ -14,6 +15,9 @@ class StoreTableSeeder extends Seeder
     protected $fillable = ['name','description','phone','mobile_phone','slug'];
     public function run()
     {
-        //
+        $stores = Store::all();
+        foreach($stores as $store){
+            $store->products()->save(Product::factory()->make());
+        }
     }
 }
